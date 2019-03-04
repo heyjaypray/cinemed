c5f7283ad83314a0bbf45b94b3e6d5b7c0a2b394
justin <hey@jaypray.com>
2019-03-04 10:54:45 -0700

    update

---
+++ components/banner/banner.js
@@ -0,0 +1,54 @@
+const banner = new Vue({
+    el: "#banner",
+    template: `
+            <section id="hero">
+				<div id="owl-main" class="owl-carousel owl-one-item height-lg">
+                    <div class="row">
+                        <div class="item img-bg-center col-lg-4 border" style="background-image: url(assets/images/cinemed/8.jpg);">
+                            <div class="container">
+                                <div class="caption vertical-center text-center">
+                                    
+                                    <h1 class="fadeInDown-1 light-color"><span>Products</span></h1>
+                                    <!-- <p class="fadeInDown-2 light-color">Create your online portfolio in minutes with the professionally and lovingly designed REEN website template. Customize your site with versatile and easy to use features.</p> -->
+                                    <!-- <div class="fadeInDown-3">
+                                        <a href="#" class="btn btn-large">Get started now</a>
+                                    </div>/.fadeIn -->
+                                    
+                                </div><!-- /.caption -->
+                            </div><!-- /.container -->
+                        </div><!-- /.item -->
+               
+           
+                        <div class="item img-bg-center col-lg-4 border" style="background-image: url(assets/images/cinemed/7.jpg);">
+                            <div class="container">
+                                <div class="caption vertical-center text-center">
+                                    
+                                    <h1 class="fadeInDown-1 light-color"><span>Services</span></h1>
+                                    <!-- <p class="fadeInDown-2 light-color">Create your online portfolio in minutes with the professionally and lovingly designed REEN website template. Customize your site with versatile and easy to use features.</p> -->
+                                    <!-- <div class="fadeInDown-3">
+                                        <a href="#" class="btn btn-large">Get started now</a>
+                                    </div>/.fadeIn -->
+                                    
+                                </div><!-- /.caption -->
+                            </div><!-- /.container -->
+                        </div><!-- /.item -->
+                
+                 
+                        <div class="item img-bg-center col-lg-4 border" style="background-image: url(assets/images/cinemed/9.jpg);">
+                            <div class="container">
+                                <div class="caption vertical-center text-center">
+                                    
+                                    <h1 class="fadeInDown-1 light-color"><span>Courses</span></h1>
+                                    <!-- <p class="fadeInDown-2 light-color">Create your online portfolio in minutes with the professionally and lovingly designed REEN website template. Customize your site with versatile and easy to use features.</p> -->
+                                    <!-- <div class="fadeInDown-3"> -->
+                                        <!-- <a href="#" class="btn btn-large">Get started now</a> -->
+                                    <!-- </div>/.fadeIn -->
+                                    
+                                </div><!-- /.caption -->
+                            </div><!-- /.container -->
+                        </div><!-- /.item -->
+                    </div>
+				</div><!-- /.owl-carousel -->
+			</section>
+    `
+})
---
+++ main.js
@@ -0,0 +1,7 @@
+import App from './components/App.vue'
+import Vue from 'vue'
+
+new Vue({
+    el: ".headerVue",
+    template: `<div><h1>FUUUUICK</h1></div>`
+  })
