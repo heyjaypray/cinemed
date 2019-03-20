dd9a029bced633a294c108aac6b7386704dd6c1a
justin <hey@jaypray.com>
2019-03-20 10:50:19 -0700

    update

--- components/courses/events.js
+++ components/courses/events.js
@@ -62,6 +62,56 @@ const events = new Vue({
 
 					</div>
 
+					<div class="row pricing">
+						
+						<div class="col-lg-4 col-md-6 inner-top-sm">
+							<div class="card" style="width: 20rem;">
+								<img class="card-img-top" src="assets/images/cinemed/1.jpg" alt="Card image cap" style="height:50%">
+								<div class="card-body">
+									<center>
+									<h4 class="card-title">10th Annual Cherry Blossom Otolaryngology Updat</h4>
+									<p class="card-text">George Washington University Hospital</p>
+									<i class="icon-globe"></i> Washington D.C. <br>
+									<i class="icon-calendar"></i> March 29-30, 2019
+									<a href="#" class="btn btn-green">View Event Site</a>
+									</center>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="col-lg-4 col-md-6 inner-top-sm">
+							<div class="card" style="width: 20rem;">
+								<img class="card-img-top" src="assets/images/cinemed/1.jpg" alt="Card image cap" style="height:50%">
+								<div class="card-body">
+									<center>
+									<h4 class="card-title">10th Annual Cherry Blossom Otolaryngology Updat</h4>
+									<p class="card-text">George Washington University Hospital</p>
+									<i class="icon-globe"></i> Washington D.C. <br>
+									<i class="icon-calendar"></i> March 29-30, 2019
+									<a href="#" class="btn btn-green">View Event Site</a>
+									</center>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-lg-4 col-md-6 inner-top-sm">
+							<div class="card" style="width: 20rem;">
+								<img class="card-img-top" src="assets/images/cinemed/1.jpg" alt="Card image cap" style="height:50%">
+								<div class="card-body">
+									<center>
+									<h4 class="card-title">10th Annual Cherry Blossom Otolaryngology Updat</h4>
+									<p class="card-text">George Washington University Hospital</p>
+									<i class="icon-globe"></i> Washington D.C. <br>
+									<i class="icon-calendar"></i> March 29-30, 2019
+									<a href="#" class="btn btn-green">View Event Site</a>
+									</center>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
 					
 						
 					
