281873a4b975a93d7059fc78b9c60d9b1b72d031
justin <hey@jaypray.com>
2019-04-08 14:01:05 -0700

    update

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,54 +1,57 @@
 const banner = new Vue({
     el: "#banner",
     template: `
+
+
             <section id="hero">
-				<div id="owl-main" class="owl-carousel owl-one-item height-lg">
-                    <div class="row">
-                        <div class="item img-bg-center col-lg-4 border" style="background-image: url(assets/images/cinemed/8.jpg);">
-                            <div class="container">
-                                <div class="caption vertical-center text-center">
-                                    
-                                    <h1 class="fadeInDown-1 light-color"><span>Products</span></h1>
-                                    <!-- <p class="fadeInDown-2 light-color">Create your online portfolio in minutes with the professionally and lovingly designed REEN website template. Customize your site with versatile and easy to use features.</p> -->
-                                    <!-- <div class="fadeInDown-3">
-                                        <a href="#" class="btn btn-large">Get started now</a>
-                                    </div>/.fadeIn -->
-                                    
-                                </div><!-- /.caption -->
-                            </div><!-- /.container -->
-                        </div><!-- /.item -->
-               
-           
-                        <div class="item img-bg-center col-lg-4 border" style="background-image: url(assets/images/cinemed/7.jpg);">
-                            <div class="container">
-                                <div class="caption vertical-center text-center">
-                                    
-                                    <a href="/services.html"><h1 class="fadeInDown-1 light-color"><span>Services</span></h1></a>
-                                    <!-- <p class="fadeInDown-2 light-color">Create your online portfolio in minutes with the professionally and lovingly designed REEN website template. Customize your site with versatile and easy to use features.</p> -->
-                                    <!-- <div class="fadeInDown-3">
-                                        <a href="#" class="btn btn-large">Get started now</a>
-                                    </div>/.fadeIn -->
-                                    
-                                </div><!-- /.caption -->
-                            </div><!-- /.container -->
-                        </div><!-- /.item -->
-                
-                 
-                        <div class="item img-bg-center col-lg-4 border" style="background-image: url(assets/images/cinemed/9.jpg);">
-                            <div class="container">
-                                <div class="caption vertical-center text-center">
-                                    
-                                    <h1 class="fadeInDown-1 light-color"><span>Courses</span></h1>
-                                    <!-- <p class="fadeInDown-2 light-color">Create your online portfolio in minutes with the professionally and lovingly designed REEN website template. Customize your site with versatile and easy to use features.</p> -->
-                                    <!-- <div class="fadeInDown-3"> -->
-                                        <!-- <a href="#" class="btn btn-large">Get started now</a> -->
-                                    <!-- </div>/.fadeIn -->
-                                    
-                                </div><!-- /.caption -->
-                            </div><!-- /.container -->
-                        </div><!-- /.item -->
-                    </div>
+				<div id="owl-main" class="owl-carousel height-md owl-inner-nav owl-ui-lg">
+					
+					<div class="item" style="background-image: url(assets/images/cinemed/8.jpg);">
+						<div class="container">
+							<div class="caption vertical-center text-center">
+								
+                            <h1 class="fadeInDown-1 light-color"><span>Lorem</span></h1>
+								<p class="fadeInDown-2 dark-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tellus id lectus blandit vestibulum sit amet in sapien. In hac habitasse platea dictumst. Nunc eget dolor felis.</p>
+								<div class="fadeInDown-3">
+									<a href="#" class="btn btn-large">Get started now</a>
+								</div><!-- /.fadeIn -->
+								
+							</div><!-- /.caption -->
+						</div><!-- /.container -->
+					</div><!-- /.item -->
+					
+					<div class="item" style="background-image: url(assets/images/cinemed/7.jpg);">
+						<div class="container">
+							<div class="caption vertical-center text-right">
+								
+								<h1 class="fadeInLeft-1 light-color"><span>Lorem</span></h1>
+								<p class="fadeInLeft-2 light-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tellus id lectus blandit vestibulum sit amet in sapien. In hac habitasse platea dictumst. Nunc eget dolor felis.</p>
+								<div class="fadeInLeft-3">
+									<a href="#" class="btn btn-large">Get started now</a>
+								</div><!-- /.fadeIn -->
+								
+							</div><!-- /.caption -->
+						</div><!-- /.container -->
+					</div><!-- /.item -->
+					
+					<div class="item" style="background-image: url(assets/images/cinemed/9.jpg);">
+						<div class="container">
+							<div class="caption vertical-center text-left">
+								
+								<h1 class="fadeInDown-1 light-color"><span>Lorem</span></h1>
+								<p class="fadeInRight-2 light-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tellus id lectus blandit vestibulum sit amet in sapien. In hac habitasse platea dictumst. Nunc eget dolor felis.</p>
+								<div class="fadeInRight-3">
+									<a href="#" class="btn btn-large">Get started now</a>
+								</div><!-- /.fadeIn -->
+								
+							</div><!-- /.caption -->
+						</div><!-- /.container -->
+					</div><!-- /.item -->
+					
 				</div><!-- /.owl-carousel -->
 			</section>
+
+
+
     `
 })
