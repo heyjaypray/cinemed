249a40ad2a2065c41fd1e72c47ad904203edc30b
justin <hey@jaypray.com>
2019-04-12 07:19:52 -0700

    update

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -8,7 +8,7 @@ const banner = new Vue({
           <div class="container">
             <div class="caption vertical-center text-center">
               
-                          <h1 class="fadeInDown-1 light-color"><span>Lorem</span></h1>
+                          <h1 class="fadeInDown-1 light-color"><span>Products</span></h1>
               <p class="fadeInDown-2 dark-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tellus id lectus blandit vestibulum sit amet in sapien. In hac habitasse platea dictumst. Nunc eget dolor felis.</p>
               <div class="fadeInDown-3">
                 <a href="#" class="btn btn-large">Get started now</a>
@@ -22,7 +22,7 @@ const banner = new Vue({
           <div class="container">
             <div class="caption vertical-center text-right">
               
-              <h1 class="fadeInLeft-1 light-color"><span>Lorem</span></h1>
+              <h1 class="fadeInLeft-1 light-color"><span>Services</span></h1>
               <p class="fadeInLeft-2 light-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tellus id lectus blandit vestibulum sit amet in sapien. In hac habitasse platea dictumst. Nunc eget dolor felis.</p>
               <div class="fadeInLeft-3">
                 <a href="#" class="btn btn-large">Get started now</a>
@@ -36,7 +36,7 @@ const banner = new Vue({
           <div class="container">
             <div class="caption vertical-center text-left">
               
-              <h1 class="fadeInDown-1 light-color"><span>Lorem</span></h1>
+              <h1 class="fadeInDown-1 light-color"><span>Courses</span></h1>
               <p class="fadeInRight-2 light-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non tellus id lectus blandit vestibulum sit amet in sapien. In hac habitasse platea dictumst. Nunc eget dolor felis.</p>
               <div class="fadeInRight-3">
                 <a href="#" class="btn btn-large">Get started now</a>
