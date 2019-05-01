7f20ad7d463e2d2a0b276e7ff1311390a47cf37f
justin <hey@jaypray.com>
2019-05-01 11:07:20 -0700

    update

--- components/products/product-list.js
+++ components/products/product-list.js
@@ -1,99 +1,75 @@
 Vue.component("product-component", {});
 
 new Vue({
-  el: ".products-list",
-  template: `
+el: ".products-list",
+template: `
 <section id="products">
   <div class="container inner-top inner-bottom-sm">
 
 
     <div class="row inner-top-sm">
 
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <a href="/product.html"><img src="/assets//images/cinemed/product1.gif"></a>
-          </div>
-          <div class="col-sm">
-            <a href="/product.html"><h4>SAGES Tips and Tricks of the Top 21</h4></a>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
               Non-Members: $390.00
             </p>
-          </div>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
         </div>
       </div>
 
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <img src="/assets//images/cinemed/product1.gif">
-          </div>
-          <div class="col-sm">
-            <h4>SAGES Tips and Tricks of the Top 21</h4>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
               Non-Members: $390.00
             </p>
-          </div>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
         </div>
       </div>
 
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <img src="/assets//images/cinemed/product1.gif">
-          </div>
-          <div class="col-sm">
-            <h4>SAGES Tips and Tricks of the Top 21</h4>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
               Non-Members: $390.00
             </p>
-          </div>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
         </div>
       </div>
 
-    </div>
-
-    <div class="row inner-top-sm">
-
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <img src="/assets//images/cinemed/product1.gif">
-          </div>
-          <div class="col-sm">
-            <h4>SAGES Tips and Tricks of the Top 21</h4>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
               Non-Members: $390.00
             </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <img src="/assets//images/cinemed/product1.gif">
-          </div>
-          <div class="col-sm">
-            <h4>SAGES Tips and Tricks of the Top 21</h4>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
-              Non-Members: $390.00
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <img src="/assets//images/cinemed/product1.gif">
-          </div>
-          <div class="col-sm">
-            <h4>SAGES Tips and Tricks of the Top 21</h4>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
-              Non-Members: $390.00
-            </p>
-          </div>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
         </div>
       </div>
 
@@ -101,46 +77,136 @@ new Vue({
 
     <div class="row inner-top-sm">
 
-
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <img src="/assets//images/cinemed/product1.gif">
-          </div>
-          <div class="col-sm">
-            <h4>SAGES Tips and Tricks of the Top 21</h4>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
               Non-Members: $390.00
             </p>
-          </div>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
         </div>
       </div>
 
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <img src="/assets//images/cinemed/product1.gif">
-          </div>
-          <div class="col-sm">
-            <h4>SAGES Tips and Tricks of the Top 21</h4>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
               Non-Members: $390.00
             </p>
-          </div>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
         </div>
       </div>
 
-      <div class="col-sm">
-        <div class="row">
-          <div class="col-sm text-right">
-            <img src="/assets//images/cinemed/product1.gif">
-          </div>
-          <div class="col-sm">
-            <h4>SAGES Tips and Tricks of the Top 21</h4>
-            <p style="font-size: 14px">SAGES Members: $320.00 <br>
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
               Non-Members: $390.00
             </p>
-          </div>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
+              Non-Members: $390.00
+            </p>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row inner-top-sm">
+
+
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
+              Non-Members: $390.00
+            </p>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
+              Non-Members: $390.00
+            </p>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
+              Non-Members: $390.00
+            </p>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem; border: none;">
+        <img class="card-img-top" src="/assets//images/cinemed/product1.gif" alt="Card image cap"
+          style="height:50%; width: 50%; margin: auto; box-shadow: 10px 10px 5px grey; ">
+        <div class="card-body">
+          <center>
+            <h5 class="card-title">SAGES Tips and Tricks of the Top 21</h5>
+          </center>
+          <center>
+            <p class="card-text">SAGES Members: $320.00 <br>
+              Non-Members: $390.00
+            </p>
+          </center>
+          <center><a href="#" class="btn btn-success">Add to Cart</a></center>
         </div>
       </div>
 
