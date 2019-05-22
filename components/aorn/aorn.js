0adc11a99ac2628314aaf098da99e66a6fada1b3
justin <hey@jaypray.com>
2019-05-22 14:24:14 -0700

    update

--- components/aorn/aorn.js
+++ components/aorn/aorn.js
@@ -1,6 +1,6 @@
 const surgery = new Vue({
-  el: "#surgery",
-  template: `
+el: "#aorn",
+template: `
 <div>
 
 	<section id="visit-our-store" class="img-bg img-bg-soft tint-bg"
@@ -10,9 +10,7 @@ const surgery = new Vue({
 			<div class="row">
 				<div class="col-lg-8 col-md-9">
 					<header>
-						<h1>Quality is Our Passion</h1>
-						<p>Magnis modipsae que voloratati andigen daepeditem quiate conecus aut labore. Laceaque quiae sitiorem rest
-							non restibusaes maio es dem tumquam explabo.</p>
+
 					</header>
 
 				</div><!-- /.col -->
@@ -37,7 +35,8 @@ const surgery = new Vue({
 
 				<div class="col">
 					<iframe width="480" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0"
-						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen></iframe>
 				</div><!-- /.col -->
 
 				<div class="col-md-6">
@@ -47,13 +46,16 @@ const surgery = new Vue({
 							<img src="assets/icons/check.png">
 						</div>
 						<div class="col">
-							<h4>Fully flexible user interface</h4>
+							<h4>Make Your Education Resources Go Further!</h4>
 							<ul>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
+								<li><i class="icon-right-open"></i>Encourage your team to watch and learn at thier own
+									pace</li>
+								<li><i class="icon-right-open"></i>The most efficient and cost-effective way to access
+									the AORN Perioperative Nursing Video Library</li>
+								<li><i class="icon-right-open"></i>Annual subscription to access the complete video
+									library</li>
+								<li><i class="icon-right-open"></i>Refreshed regularly with new topics and updates to
+									existing titled</li>
 							</ul>
 						</div>
 					</div>
@@ -62,50 +64,58 @@ const surgery = new Vue({
 							<img src="assets/icons/check.png">
 						</div>
 						<div class="col">
-							<h4>Fully flexible user interface</h4>
+							<h4>User-Friendly Interface</h4>
 							<ul>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
+								<li><i class="icon-right-open"></i>Each video comes with study guides and posttests</li>
+								<li><i class="icon-right-open"></i>Score tracking and posttest completion certificates
+								</li>
+								<li><i class="icon-right-open"></i>Assign videos at user level</li>
+								<li><i class="icon-right-open"></i>Categories include
+
+									Aseptic Practices,
+									Equipment and Product Safety,
+									Patient and Worker Safety,
+									Perioperative Patient Care Concepts,
+									Sterilization and Disinfection,
+									Aseptic Practices,
+									Management Concepts,
+									Specialty Perioperative Education
+
+								</li>
+
 							</ul>
 						</div>
 					</div>
-					<div class="row">
-						<div class="col-md-2">
-							<img src="assets/icons/check.png">
-						</div>
-						<div class="col">
-							<h4>Fully flexible user interface</h4>
-							<ul>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-								<li><i class="icon-right-open"></i>Vivae modipsae voloratati Vivae modipsae voloratati</li>
-							</ul>
-						</div>
-					</div>
+
 
 
 				</div>
 
 			</div>
-			<div class="row">
+			<div class="row mt-5">
+
+				<div class="col-md-6 inner-top-xs inner-left-xs">
+					<h2>Who Are The Videos Designed For?</h2>
+					<ul class="circled">
+						<li>Registered Nurses currently enrolled in Periop 101</li>
+						<li>New and experienced registered nurses transferring to the perioperative suite from other
+							nursing specialties</li>
+						<li>Sterile processing team members</li>
+						<li>Non-RN perioperative team members (e.g. surgical technologist, surgical assistants/aids)
+						</li>
+						<li>Other RNs involved in operative/invasive procedures in non-traditional OR settings (e.g.
+							Endoscopy, OB, Cath Lab, Radiology)</li>
+						<li>Nursing students at schools of nursing</li>
+						<li>Nurse managers interested in leveraging video content for in-services</li>
+					</ul>
+					<a href="#" class="txt-btn">Check out the functions</a>
+				</div><!-- /.col -->
 
 				<div class="col-md-6 inner-right-xs text-right">
 					<figure><img src="assets/images/art/product01.jpg" alt=""></figure>
 				</div><!-- /.col -->
 
-				<div class="col-md-6 inner-top-xs inner-left-xs">
-					<h2>Fully flexible user interface</h2>
-					<p>Magnis modipsae que lib voloratati andigen daepeditem quiate ut reporemni aut labor. Laceaque quiae
-						sitiorem rest non restibusaes es tumquam core posae volor remped modis volor. Doloreiur qui commolu ptatemp
-						dolupta oreprerum tibusam. Eumenis et consent accullignis dentibea autem inisita posae volor conecus resto
-						explabo.</p>
-					<a href="#" class="txt-btn">Check out the functions</a>
-				</div><!-- /.col -->
+
 
 
 			</div><!-- /.row -->
@@ -124,10 +134,10 @@ const surgery = new Vue({
 			<div class="row">
 				<div class="col-lg-8 col-md-9 mx-auto text-center">
 					<header>
-						<h1>What drives us</h1>
-						<p>Magnis modipsae que voloratati andigen daepedor quiate reporemni autem. Laceaque quiae sitiorem
-							restibusaes es novum tumquam core posae volor remped modis volor dolor qui commolu ptatemp dolupta orem
-							retibusam emnis.</p>
+						<h1>What Drives Us</h1>
+						<p>Cine-Med and AORN partner to deliver a convient online program that allows perioperative
+							educators to establish and deliver a standardized evidence-based education program for
+							novice perioperative nurses</p>
 					</header>
 				</div><!-- /.col -->
 			</div><!-- /.row -->
@@ -141,9 +151,8 @@ const surgery = new Vue({
 				</div><!-- /.col -->
 
 				<div class="col-lg-3 col-sm-10 col-9 inner-bottom-xs">
-					<h2>Passion</h2>
-					<p class="text-small">Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor.
-						Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+					<h2>Accessibility</h2>
+					<p>Cost-Efficient Online Access to Perioperative Nurse Training</p>
 				</div><!-- /.col -->
 
 				<div class="col-lg-1 col-sm-2 col-3 text-right">
@@ -153,9 +162,8 @@ const surgery = new Vue({
 				</div><!-- /.col -->
 
 				<div class="col-lg-3 col-sm-10 col-9 inner-bottom-xs">
-					<h2>Creativity</h2>
-					<p class="text-small">Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor.
-						Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+					<h2>Mastery</h2>
+					<p class="text-small">Content-Rich information to stengthen knowledge and skills</p>
 				</div><!-- /.col -->
 
 				<div class="col-lg-1 col-sm-2 col-3 text-right">
@@ -166,8 +174,8 @@ const surgery = new Vue({
 
 				<div class="col-lg-3 col-sm-10 col-9 inner-bottom-xs">
 					<h2>Quality</h2>
-					<p class="text-small">Magnis modipsae que lib voloratati andigen daepeditem quiate re porem aut labor.
-						Laceaque quiae sitiorem rest non restibusaes maio es dem tumquam core posae volor remped modis volor.</p>
+					<p class="text-small">Based on AORN's latest evidence-based Guidelines for Perioperative Practice
+					</p>
 				</div><!-- /.col -->
 
 			</div><!-- /.row -->
@@ -183,7 +191,8 @@ const surgery = new Vue({
 				<div class="col-lg-8 col-md-9 mx-auto text-center">
 					<header>
 						<h1>Simple pricing, no surprises</h1>
-						<p>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest
+						<p>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae
+							sitiorem rest
 							non restibusaes maio es dem tumquam.</p>
 					</header>
 				</div><!-- /.col -->
@@ -323,8 +332,10 @@ const surgery = new Vue({
 
 						<div class="item">
 							<blockquote>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed arcu eros. Lorem ipsum dolor sit
-									amet, consectetur adipiscing elit. Vivamus ultrices, elit et tempus mollis, velit ipsum rutrum nunc,
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed arcu eros. Lorem
+									ipsum dolor sit
+									amet, consectetur adipiscing elit. Vivamus ultrices, elit et tempus mollis, velit
+									ipsum rutrum nunc,
 									id auctor tellus dolor sed enim.</p>
 								<footer>
 									<cite>Steve Gates</cite>
@@ -334,8 +345,10 @@ const surgery = new Vue({
 
 						<div class="item">
 							<blockquote>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed arcu eros. Lorem ipsum dolor sit
-									amet, consectetur adipiscing elit. Vivamus ultrices, elit et tempus mollis, velit ipsum rutrum nunc,
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed arcu eros. Lorem
+									ipsum dolor sit
+									amet, consectetur adipiscing elit. Vivamus ultrices, elit et tempus mollis, velit
+									ipsum rutrum nunc,
 									id auctor tellus dolor sed enim.</p>
 								<footer>
 									<cite>Amber Jones</cite>
@@ -345,8 +358,10 @@ const surgery = new Vue({
 
 						<div class="item">
 							<blockquote>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed arcu eros. Lorem ipsum dolor sit
-									amet, consectetur adipiscing elit. Vivamus ultrices, elit et tempus mollis, velit ipsum rutrum nunc,
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed arcu eros. Lorem
+									ipsum dolor sit
+									amet, consectetur adipiscing elit. Vivamus ultrices, elit et tempus mollis, velit
+									ipsum rutrum nunc,
 									id auctor tellus dolor sed enim.</p>
 								<footer>
 									<cite>Bill Jobs</cite>
